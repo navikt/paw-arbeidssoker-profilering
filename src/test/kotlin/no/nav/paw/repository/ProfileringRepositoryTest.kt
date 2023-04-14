3a887ba38082effe9aebdf6a3a57b8dbdc3df955
Jonas Enge <jonas.maccyber.enge@nav.no>
2023-04-14 10:28:45 +0200

    Endrer modell for lagring og kafka melding

--- src/test/kotlin/no/nav/paw/repository/ProfileringRepositoryTest.kt
+++ src/test/kotlin/no/nav/paw/repository/ProfileringRepositoryTest.kt
@@ -1,18 +1,12 @@
 package no.nav.paw.repository
 
 import no.nav.paw.ProfileringTestData
-import no.nav.paw.config.Config
-import no.nav.paw.createConfig
 import no.nav.paw.utils.TestDatabase
 import no.nav.paw.utils.TestDatabase.setupDataSource
-import no.nav.paw.utils.TestKafka
-import no.nav.security.mock.oauth2.MockOAuth2Server
 import org.junit.jupiter.api.AfterAll
 import org.junit.jupiter.api.BeforeAll
-import org.testcontainers.containers.KafkaContainer
 import org.testcontainers.containers.PostgreSQLContainer
 import javax.sql.DataSource
-import kotlin.test.BeforeTest
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
